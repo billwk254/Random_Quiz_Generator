f3fe48a31385ccb075263d5915702db296f260ee
billwk254 <billkihungu51@gmail.com>
2022-04-05 15:51:19 +0300

    PYTHON FILE WHICH GENERATES A DICTIONARY WITH A KEY AND VALUE PAIRING OF COUNTRY AND ITS CORRESPONDING CAPITAL CITY WITH THE CONTENTS BEING DRAWN FROM THE COUNTRYINFO.PY FILE

---
+++ countries_and_capitals_generator.py
@@ -0,0 +1,21 @@
+from countryinfo import countries
+
+countryinfo = countries
+
+#Get a list of capital cities from the countryinfo.py file
+capitals = [d['capital']for d in countryinfo]
+
+#Get a list of countries from the countryinfo.py file
+countrynames = [z["name"] for z in countryinfo]
+
+
+#Generate  a dictionary of values from the two lists generated above
+
+country_capitals = {}
+
+for i in range(0,len(countrynames)):
+    country_capitals[countrynames[i]] = capitals[i]
+
+
+
+
