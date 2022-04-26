9d9924e39f6bb628c311e539cce9477e0b7b4a28
billwk254 <billkihungu51@gmail.com>
2022-04-26 13:48:21 +0300

    1. ADDED IN LOGIC TO ALLOW THE USER TO SPECIFY A CUSTOM LOCATION TO STORE THE FILES GENERATED BY THE PROGRAM 2. ADDED IN LOGIC TO GENERATE A QUIZ  WITH COUNTRIES FROM A CONTINENT SPECIFIED BY THE USER 3. ADDED IN LOGIC TO ALLOW THE USER TO SPECIFY THE NUMBER OF STUDENTS BEING TESTED FOR THE PROGRAM TO GENERATE THE EXACT NUMBER OF FILES FOR EACH STUDENT 4. ADDED IN LOGIC SUCH THAT WHEN THE USER USES THE SAME FOLDER TWICE TO GENERATE QUIZ FILES, THE PROGRAM CAN DELETE ANY LEFT-OVER FILES FROM THE FIRST RUN OF THE PROGRAM IF A USER SPECIFIES A LESSER NUMBER OF STUDENTS TO TEST ON THE SECOND RUN OF THE PROGRAM

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+#import all the required packages
 import random
 import os
-from countries_and_capitals_generator import country_capitals as capitals
+from countries_and_capitals_generator import country_capitals
+from generate_by_continents import continentselector
+from generate_by_continents import country_capitals2
+import re
+
+#a dictionary with states in the US and their state capitals
 """
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
@@ -19,19 +25,65 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
 """
+#regular expression to help in identifying when there are more quiz files than the number requested by the user
+questionfileregex = re.compile(r"(capitalsquiz)(\d?\d)")
+answerfileregex =  re.compile(r"(capitalsquiz_answers)(\d?\d)")
+
+# a function to let the user input the number of students being tested
+def num_of_students():
+    global studentnum
+    while True:
+        studentnum = int(input("How many students are you planning to test: \n"))
+        if studentnum:
+            break
+
+num_of_students()
 
 #creates a new folder for putting in all our files in if it does not exist
+def userpath():
+    global questionfolder, answerfolder,userpath
+    while True:
+        userpath = os.path.abspath(input("Please enter a path to use to store the files: \n"))
+        if os.path.isdir(userpath) and not os.path.exists(userpath):
+            questionfolder = os.path.join(userpath,"world_capitals_quiz\\question_sheets\\")
+            answerfolder = os.path.join(userpath,"world_capitals_quiz\\answer_sheets\\")
+            os.makedirs(questionfolder)
+            os.makedirs(answerfolder)
+            break
+        elif os.path.exists(userpath):
+            break
+        else:
+            continue
 
-if os.path.exists("C:\\Users\\billk\\Desktop\\us_state_capitals_quiz"):
-    pass
-else:
-    os.makedirs("C:\\Users\\billk\\Desktop\\us_state_capitals_quiz\\question_sheets")
-    os.makedirs("C:\\Users\\billk\\Desktop\\us_state_capitals_quiz\\answer_sheets")
+userpath()
 
-for quiznum in range(35):
+#lists for storing all the excess files in the question and answer folders
+excessquestionfiles = []
+excessanswerfiles = []
+
+#a function which lets the user select a specific continent to draw the quiz from or generate a quiz with all countries
+def quiz_type():
+    while True:
+        global capitals
+        user_select =  input("Do you want to generate a quiz for a specific continent(C) or for all countries(A): \n")
+        if user_select:
+            if user_select.lower() == "c":
+                continentselector()
+                capitals = country_capitals2
+                break
+            elif user_select.lower() == "a":
+                capitals = country_capitals
+                break
+            else:
+                continue
+
+quiz_type()
+
+
+for quiznum in range(studentnum):
     # create the quiz and answer key files
-    quizfile = open("C:\\Users\\billk\\Desktop\\us_state_capitals_quiz\\question_sheets\\capitalsquiz%s.txt" % (quiznum + 1), "w")
-    answerkeyfile = open("C:\\Users\\billk\\Desktop\\us_state_capitals_quiz\\answer_sheets\\capitalsquiz_answers%s.txt" % (quiznum + 1), "w")
+    quizfile = open(f"{userpath}\\world_capitals_quiz\\question_sheets\\capitalsquiz%s.txt" % (quiznum + 1), "w")
+    answerkeyfile = open(f"{userpath}\\world_capitals_quiz\\answer_sheets\\capitalsquiz_answers%s.txt" % (quiznum + 1), "w")
 
     #write out the header for the quiz
     quizfile.write("Name:\n\nDate:\n\nClass\n\n")
@@ -43,7 +95,7 @@ for quiznum in range(35):
     random.shuffle(countries)
 
     #get right and wrong answers
-    for questionnum in range(193):
+    for questionnum in range(0,len(capitals)):
         correctanswer = capitals[countries[questionnum]]
         wronganswer = list(capitals.values())
         del wronganswer[wronganswer.index(correctanswer)]
@@ -62,6 +114,29 @@ for quiznum in range(35):
         answerkeyfile.write("%s.%s\n" % (questionnum +1, "ABCD"[answeroptions.index(correctanswer)]))
     quizfile.close()
     answerkeyfile.close()
+
+#function to delete all excess files found in the folders where the quiz and its answer files are being generated
+def delete_overflow():
+    for files in os.listdir(f"{userpath}\\world_capitals_quiz\\question_sheets"):
+        if files.endswith(".txt"):
+            exquestions = questionfileregex.search(files)
+            if int(exquestions.group(2)) > studentnum:
+                excessquestionfiles.append(files)
+    for ofiles in os.listdir(f"{userpath}\\world_capitals_quiz\\answer_sheets\\"):
+        if files.endswith(".txt"):
+            exanswers = answerfileregex.search(ofiles)
+            if int(exanswers.group(2)) > studentnum:
+                excessanswerfiles.append(ofiles)
+    for exq in excessquestionfiles:
+        delfilelocation = os.path.join(f"{userpath}\\world_capitals_quiz\\question_sheets", exq)
+        os.remove(delfilelocation)
+    for exa in excessanswerfiles:
+        delfilelocation2 = os.path.join(f"{userpath}\\world_capitals_quiz\\answer_sheets\\", exa)
+        os.remove(delfilelocation2)
+
+delete_overflow()
+
+print("THE TEST AND ANSWER FILES FOR " + str(studentnum) + f" STUDENTS HAS BEEN STORED TO THE FOLLOWING LOCATION \n\n ********{userpath}********* ")
 
 
 
